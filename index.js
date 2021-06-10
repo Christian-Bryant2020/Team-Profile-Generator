861b5dbd68a7ddbbac89b999df9a2fc5f8b14f55
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-10 16:31:24 -0500

    Set up Employee/manager lib

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const Intern = require('./lib/intern');
-const Engineer = require('./lib/engineer');
+// const Intern = require('./lib/intern');
+// const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Employee = require('./lib/employee');
+const createProfile = require('./lib/profile')
+
+createProfile.createProfile();
