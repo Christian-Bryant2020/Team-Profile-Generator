5f4ad27e28cf50cb493a2c13e913f1f1ffc0faed
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-10 16:40:28 -0500

    Addition of template helper

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const Intern = require('./lib/intern');
-// const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Employee = require('./lib/employee');
 const createProfile = require('./lib/profile')
