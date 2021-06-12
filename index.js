e2e32f11c46c86e7f182360559fef968a67c360b
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-11 20:39:07 -0500

    TemplateHelper update

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ const Manager = require('./lib/manager');
 const Employee = require('./lib/employee');
 const createProfile = require('./lib/profile')
 
+async function init () {
+    await createProfile();
+     
+}
+
+
+
 createProfile.createProfile();
+
+async function writeFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if (err) {
+            console.error(err)
+        }
+        console.log('Your HTML file has been generated!');
+    });
+};
+
+init();
--- src/templateHelper.js
+++ src/templateHelper.js
@@ -1,8 +1,22 @@
 
-function generatePage() {
-
-console.log('generating HTML')
-
-};
+function generatePage(data)  {
+    console.log('Generating HTML')
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <p>Test</p>
+</body>
+</html>
+    
+    ` 
+}
 
 module.exports= generatePage;
