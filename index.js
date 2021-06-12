29bd6d92b55bb373d96cca1e04f8952eff27a126
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-12 16:42:59 -0500

    HTML generation

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const Intern = require('./lib/intern');
-const Engineer = require('./lib/engineer');
-const Manager = require('./lib/manager');
-const Employee = require('./lib/employee');
 const createProfile = require('./lib/profile')
 
-async function init () {
-    await createProfile();
-     
-}
-
-
-
 createProfile.createProfile();
-
-async function writeFile(filename, data) {
-    fs.writeFile(filename, data, (err) => {
-        if (err) {
-            console.error(err)
-        }
-        console.log('Your HTML file has been generated!');
-    });
-};
-
-init();
--- src/templateHelper.js
+++ src/templateHelper.js
@@ -1,22 +1,30 @@
+let answers = generatePage
+const fs = require('fs');
 
-function generatePage(data)  {
-    console.log('Generating HTML')
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Team Profile Generator</title>
-</head>
-<body>
-    <p>Test</p>
-</body>
-</html>
+  let html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="./style.css" rel="stylesheet">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <h1 class="ms-auto me-auto p-4">Profile</h1>
+        </nav>
+        <div class="container">
+            <div class="row row-cols-auto">   
+                </div>    
+        </div>
+    </body>
+    </html>`;
     
-    ` 
-}
+    function generatePage(answers) {
+        console.log(answers)
+    }
+        
 
 module.exports= generatePage;
